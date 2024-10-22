850cf23905e2b3f25a5f82a2a48f4a00cf2cacdd
Neeraj Shrivas <shrivasneeraj01@gmail.com>
2024-10-22 21:40:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
+const table = document.getElementById("myTable");
+
 function createTable() {
     //Write your code here
-  
+	
+	let row = prompt("Input number of rows")
+	let column = prompt("Input number of columns")
+	for (let i = 0; i < row; i++) {
+		let tr = document.createElement("tr");
+		
+		for (let j = 0; j < column; j++) {
+			tr.innerHTML += `
+				<td>Row-${i} Column-${j}</td>
+				`;
+		}
+		table.append(tr)
+	}
+	
 }
+
+
+// btn.addEventListener("click", (e) => {
+// 	e.preventDetails();
+	
+// 	createTable(row, column)
+// })
